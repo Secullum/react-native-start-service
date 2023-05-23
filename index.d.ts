1f12324e059c0405a5b052fb137618022832ffdd
Denis Weber <denis.weber@secullum.com.br>
2023-05-23 15:05:49 -0300

    Add isRunningServiceAsync

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 export function isPackageInstalledAsync(packageName: string): Promise<boolean>;
 
+export function isRunningServiceAsync(serviceName: string): Promise<boolean>;
+
 export function startAsync<TResult>(
   packageName: string,
   className: string,
